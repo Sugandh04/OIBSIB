0bc16fb088864747ed6a64f2cb63fc7418560cbc
sugandh04 <sugandh.ishaan@gmail.com>
2026-03-10 17:11:04 +0530

    Formatted Document

--- Task2_Sundarpichai/script.js
+++ Task2_Sundarpichai/script.js
@@ -2,15 +2,15 @@ const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
 
-reveals.forEach(section => {
+    reveals.forEach(section => {
 
-const windowHeight = window.innerHeight;
-const elementTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        const elementTop = section.getBoundingClientRect().top;
 
-if(elementTop < windowHeight - 100){
-section.classList.add("active");
-}
+        if (elementTop < windowHeight - 100) {
+            section.classList.add("active");
+        }
 
-});
+    });
 
 });
